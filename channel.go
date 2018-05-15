c67b9a68bd0eb1aa3af72098b090ca72b18795d3
riking <rikingcoding@gmail.com>
2018-05-15 14:18:29 -0700

    don't error out when we fail pins fetch....?

--- channel.go
+++ channel.go
@@ -72,7 +72,7 @@ func (c *ManagedChannel) LoadBacklog() error {
 	pins, err := c.bot.s.ChannelMessagesPinned(c.Channel.ID)
 	if err != nil {
 		fmt.Println("could not load pins for", c.Channel.ID, err)
-		return err
+		// return err
 	}
 	fmt.Println("backlog for", c.Channel.ID, "len =", len(msgs))
 
