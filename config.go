6efe7bc51630b3caa5e054e861a7b60320ce1ae9
Kane York <kanepyork@google.com>
2019-08-16 19:57:19 -0700

    typo

--- config.go
+++ config.go
@@ -43,7 +43,7 @@ type Config struct {
 	ClientID     string `yaml:"clientid"`
 	ClientSecret string `yaml:"clientsecret"`
 	BotToken     string `yaml:"bottoken"`
-	ShardCount   int    `yaml:"shards"`
+	Shards       int    `yaml:"shards"`
 	ErrorLogCh   string `yaml:"errorlog"`
 	HTTP         struct {
 		Listen string `yaml:"listen"`
