16d865614c280afe8d9dd3736689c78fc812f4b2
Kane York <rikingcoding@gmail.com>
2019-12-22 15:41:28 -0800

    document production build.sh file

---
+++ docs/build.sh
@@ -0,0 +1,1 @@
+(cd "$(go env GOPATH)/src/github.com/riking/AutoDelete"; git pull); go build -ldflags="-s -w" -v -o ~/go/bin/autodelete github.com/riking/AutoDelete/cmd/autodelete ; cp ~/go/bin/autodelete ./autodelete
