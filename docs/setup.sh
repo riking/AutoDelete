2f90d75131093d95079292622c21551790b71541
Kane York <rikingcoding@gmail.com>
2019-12-22 15:54:40 -0800

    Add instructions for setting up self-hosting

---
+++ docs/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Do not run this script directly; read it and copy out commands as appropriate."; cat $0; exit 0
+
+# Check that go is installed
+
+go version || echo "https://golang.org/dl/"
+
+# Fetch the code
+
+go get -u -v github.com/riking/AutoDelete/cmd/autodelete
+
+# Create a data folder
+
+FOLDER=$HOME/autodelete
+mkdir -p "$FOLDER" ; cd "$FOLDER"
+
+# Set up: fetch config.yml, build.sh, create a 'data' folder
+
+cp "$(go env GOPATH)/src/github.com/riking/AutoDelete/docs/build.sh" .
+cp "$(go env GOPATH)/src/github.com/riking/AutoDelete/config.example.yml" config.yml
+mkdir data
+
+# Rebuild
+
+./build.sh
+
+# Run
+
+./autodelete
+
