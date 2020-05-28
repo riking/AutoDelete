501c722d9abf518df2bc1a3f3a5f54101c2fd6b7
Kane York <rikingcoding@gmail.com>
2020-05-27 20:53:03 -0700

    improve identify_channels

--- cmd/identify_channels.go
+++ cmd/identify_channels.go
@@ -43,12 +43,17 @@ func main() {
 		return
 	}
 
+	state := discordgo.NewState()
+	s.State = state
+
 	for _, guildID := range flag.Args() {
 		guild, err := s.Guild(guildID)
 		if err != nil {
 			fmt.Printf("error fetching %s: %v", guildID, err)
 			continue
 		}
+		fetchRoles(s, guild)
+
 		guildIDNumeric, err := strconv.ParseInt(guildID, 10, 64)
 		if err != nil {
 			panic(err)
@@ -66,14 +71,21 @@ func main() {
 			fmt.Printf("error fetching channels: %v", err)
 		}
 		for _, v := range channelList {
-			if *flagUnconfiguredChannels || isConfigured(v.ID) {
+			if (*flagUnconfiguredChannels && v.Type != discordgo.ChannelTypeGuildVoice) || isConfigured(v.ID) {
 				fmt.Printf("  %s: #%s\n", v.ID, v.Name)
+				var managers, denied []*discordgo.PermissionOverwrite
 				for _, perm := range v.PermissionOverwrites {
 					if 0 != (perm.Allow & discordgo.PermissionManageMessages) {
-						fmt.Printf("    %s %s: perms +%x -%x\n", perm.Type, perm.ID, perm.Allow, perm.Deny)
+						managers = append(managers, perm)
 					} else if 0 != perm.Deny {
-						fmt.Printf("    %s %s: perms +%x -%x\n", perm.Type, perm.ID, perm.Allow, perm.Deny)
+						denied = append(denied, perm)
 					}
+				}
+				for _, v := range managers {
+					printInterestingPermissions(s, guild, v)
+				}
+				for _, v := range denied {
+					printInterestingPermissions(s, guild, v)
 				}
 			}
 		}
@@ -90,5 +102,39 @@ func isConfigured(channelID string) bool {
 	} else {
 		fmt.Printf("[ERR] %v", err)
 		return false
+	}
+}
+
+var roleMap = make(map[string]*discordgo.Role)
+
+func fetchRoles(s *discordgo.Session, g *discordgo.Guild) {
+	roles, err := s.GuildRoles(g.ID)
+	if err != nil {
+		fmt.Printf("err fetching roles(%s): %v", g.ID, err)
+		return
+	}
+	for _, v := range roles {
+		roleMap[v.ID] = v
+	}
+}
+
+func printInterestingPermissions(s *discordgo.Session, g *discordgo.Guild, perm *discordgo.PermissionOverwrite) {
+	switch perm.Type {
+	case "member":
+		user, err := s.User(perm.ID)
+		if err == nil {
+			fmt.Printf("   user %s (%s): perms +%x -%x\n", user, perm.ID, perm.Allow, perm.Deny)
+		} else {
+			fmt.Printf("   user %s (err %v): perms +%x -%x\n", perm.ID, err, perm.Allow, perm.Deny)
+		}
+	case "role":
+		role, ok := roleMap[perm.ID]
+		if ok {
+			fmt.Printf("   role %s (%s): perms +%x -%x\n", role.Name, perm.ID, perm.Allow, perm.Deny)
+		} else {
+			fmt.Printf("   role %s (err): perms +%x -%x\n", perm.ID, perm.Allow, perm.Deny)
+		}
+	default:
+		fmt.Printf("   %s %s: perms +%x -%x\n", perm.Type, perm.ID, perm.Allow, perm.Deny)
 	}
 }
